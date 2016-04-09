e409c4d533c06d5cca3e2b013be9bbea1483b568
Jakub Valenta <jakub@jakubvalenta.cz>
2016-04-09 19:16:36 +0200

    mpv: Play in xterm in fullscreen to catch keyboard
    And fix hwdec config.

--- src/home/alarm/bin/devmon-play-mpv.sh
+++ src/home/alarm/bin/devmon-play-mpv.sh
@@ -2,7 +2,7 @@
 
 mkdir -p "$HOME/.log/"
 
-/usr/bin/devmon --exec-on-drive "killall mpv; $HOME/bin/play-dir-mpv.sh \"%d\""\
+/usr/bin/devmon --exec-on-drive "killall mpv; xterm -fullscreen -e $HOME/bin/play-dir-mpv.sh \"%d\""\
                 --exec-on-unmount "killall mpv"\
                 --exec-on-remove "killall mpv"\
-                --always-exec &> "$HOME/.log/devmon.log"\
+                --always-exec &> "$HOME/.log/devmon.log"
